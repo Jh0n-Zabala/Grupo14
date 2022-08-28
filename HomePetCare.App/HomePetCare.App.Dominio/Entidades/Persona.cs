aa15dcc8bf62b75ffa29ef31f8503a2765108544
dvargasp <dvargasp@intergrupo.com>
2022-08-27 21:55:07 -0500

    configuracion inicial 2

---
+++ HomePetCare.App/HomePetCare.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,12 @@
+namespace HomePetCare.App.Dominio
+{
+public class Persona
+	{
+		public int Id { get; set; }
+		public String Nombre{get;set;}
+		public String Apellidos{get;set;}
+		public String NumeroTelefono{get;set;}
+        public Genero Genero { set; get; }
+	}
+}
+	
